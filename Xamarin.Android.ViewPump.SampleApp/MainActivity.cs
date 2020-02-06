4fde1f8c796dbcbd0f64ce58dc8927f08464be5f
Andrii Petrovskyi <andrew.petrowski@outlook.com>
2020-02-06 23:07:05 +0200

    Add project files.

---
+++ Xamarin.Android.ViewPump.SampleApp/MainActivity.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using IO.Github.Inflationx.Viewpump;
+
+namespace Xamarin.Android.ViewPump.SampleApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void AttachBaseContext(Context @base)
+        {
+           // base.AttachBaseContext(@base);
+           base.AttachBaseContext(ViewPumpContextWrapper.Wrap(@base));
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            var builder = IO.Github.Inflationx.Viewpump.ViewPump.InvokeBuilder();
+            builder.AddInterceptor(new TextUpdateInterceptor());
+            IO.Github.Inflationx.Viewpump.ViewPump.Init(builder.Build());
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            global::Android.Support.V7.Widget.Toolbar toolbar = FindViewById<global::Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (global::Android.Views.View.IOnClickListener)null).Show();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+
+    public class TextUpdateInterceptor : Java.Lang.Object, IInterceptor
+    {
+        public InflateResult Intercept(IInterceptorChain p0)
+        {
+            var result = p0.Proceed(p0.Request());
+
+            if (result.View() is TextView textView)
+            {
+                textView.Text = "[Prefix]: " + textView.Text;
+            }
+
+            return result;
+        }
+    }
+}
+
